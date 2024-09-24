04358bdc824f870346b7c30ece4f8052f087c537
Antonio Mendoza <mendoza.a01@outlook.com>
2024-09-24 12:58:54 -0400

    updated implementation and images from running attack on image from dataset

--- implementation.py
+++ implementation.py
@@ -1,19 +1,30 @@
 import torch
+import torchvision
 import torch.nn as nn
 import torch.optim as optim
-import torchvision
 import matplotlib.pyplot as plt
+import numpy as np
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+from torchvision.utils import save_image
 
-# define out data model CIFAR10
-class CIFAR10_model(nn.Module):
-    # stuff to define data model
-    def __init__(self):
-        super(CIFAR10Model, self).__init__()
+# define transforms
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
 
-# initialize CIFAR10
-model = CIFAR10Model()
+test_data =datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
 
-# og_data = take an image from data model to compare random noise to
+test_load = DataLoader(test_data, batch_size=128, shuffle=False)
+
+# Get one image from the dataset
+data_iter = iter(test_load)
+image, label = next(data_iter)
+target = image[127].unsqueeze(0)
+
+# save the image
+save_image(target, 'target.png')
 
 # initialize random data using Gaussian Distribution
 rand_data = torch.normal(0.5, 0.1, size=(1, 3, 32, 32), requires_grad=True)
@@ -22,14 +33,19 @@ rand_data = torch.normal(0.5, 0.1, size=(1, 3, 32, 32), requires_grad=True)
 loss_func = nn.MSELoss()
 
 # define optimize function using sgd with learning rate 0.1
-optimize = optim.SGD([input], lr=0.1)
+optimize = optim.SGD([rand_data], lr=0.1)
+
+# number of steps
+steps = 100000
 
 # loop for optimizition
-for step in range(100):
+for step in range(steps):
     optimize.zero_grad() # reset gradient
-    output = model(input) # 
-    loss = loss_func(output, model(og_data)) # computing loss between outputs
+    loss = loss_func(rand_data, target) # computing loss between outputs
     loss.backward() # backward pass for computing gradients
     optimize.step() # update the parameters based on gradients
 
-    print(f"step: {step}, Loss: {loss.item()}")
+    if (step % 100) == 0:
+        print(f"step: {step}, Loss: {loss.item()}")
+
+save_image(rand_data, 'recovered_img.png')
