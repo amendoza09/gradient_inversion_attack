683d49ccc9e4ac191bc5a63f1b8d9ffd8aabad68
Jaewoo Lee <jwlee3757@gmail.com>
2024-09-25 08:37:51 -0400

    needs to implement gradient matching

--- implementation.py
+++ implementation.py
@@ -8,44 +8,52 @@ from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 from torchvision.utils import save_image
 
-# define transforms
-transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-])
 
-test_data =datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+def main():
+    # define transforms
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
 
-test_load = DataLoader(test_data, batch_size=128, shuffle=False)
+    test_data = datasets.CIFAR10(root='./data',
+                                 train=False,
+                                 download=True,
+                                 transform=transform)
+    test_load = DataLoader(test_data, batch_size=128, shuffle=False)
 
-# Get one image from the dataset
-data_iter = iter(test_load)
-image, label = next(data_iter)
-target = image[127].unsqueeze(0)
+    # Get one image from the dataset
+    data_iter = iter(test_load)
+    image, label = next(data_iter)
+    target = image[127].unsqueeze(0)
 
-# save the image
-save_image(target, 'target.png')
+    # save the image
+    save_image(target, 'target.png')
 
-# initialize random data using Gaussian Distribution
-rand_data = torch.normal(0.5, 0.1, size=(1, 3, 32, 32), requires_grad=True)
+    # initialize random data using Gaussian Distribution
+    rand_data = torch.normal(0.5, 0.1, size=(1, 3, 32, 32), requires_grad=True)
 
-# define loss funciton using L2 norm or Mean Squared Error
-loss_func = nn.MSELoss()
+    # define loss funciton using L2 norm or Mean Squared Error
+    loss_func = nn.MSELoss()
 
-# define optimize function using sgd with learning rate 0.1
-optimize = optim.SGD([rand_data], lr=0.1)
+    # define optimize function using sgd with learning rate 0.1
+    optimize = optim.SGD([rand_data], lr=0.1)
 
-# number of steps
-steps = 100000
+    # number of steps
+    steps = 100000
 
-# loop for optimizition
-for step in range(steps):
-    optimize.zero_grad() # reset gradient
-    loss = loss_func(rand_data, target) # computing loss between outputs
-    loss.backward() # backward pass for computing gradients
-    optimize.step() # update the parameters based on gradients
+    # loop for optimizition
+    for step in range(steps):
+        optimize.zero_grad()  # reset gradient
+        loss = loss_func(rand_data, target)  # computing loss between outputs
+        loss.backward()  # backward pass for computing gradients
+        optimize.step()  # update the parameters based on gradients
 
-    if (step % 100) == 0:
-        print(f"step: {step}, Loss: {loss.item()}")
+        if (step % 100) == 0:
+            print(f"step: {step}, Loss: {loss.item()}")
 
-save_image(rand_data, 'recovered_img.png')
+    save_image(rand_data, 'recovered_img.png')
+
+
+if __name__ == "__main__":
+    main()
